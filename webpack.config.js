caf230d4b6735a3c15dcc2849376590e2e9124a2
helloyou2012 <helloyou2012@yeah.net>
2017-05-17 10:17:52 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,36 @@ const jsonServer = require('json-server');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (config, webpack) => {
+  config.output.publicPath = 'http://localhost:8000/';
+  // 添加 CommonsChunkPlugin
   const plugin = new webpack.optimize.CommonsChunkPlugin({
     name: 'vendor'
   });
   config.plugins.push(plugin);
-  config.babel.plugins = ['transform-runtime'];
-  config.output.publicPath = 'http://localhost:8000/';
+
+  const babel = { loader: 'babel-loader' };
+  // 添加 babel plugin
+  config.module.rules.forEach(rule => {
+    if (rule.key === 'js' || rule.key === 'jsx') {
+      rule.options.plugins = [
+        'transform-runtime'
+      ];
+      babel.options = rule.options;
+    }
+  });
+  // 添加 vue-loader
   config.module.rules.push({
     test: /\.vue$/,
     loader: 'vue-loader',
     options: {
-      postcss: config.postcss,
+      extractCSS: true,
+      postcss: [require('autoprefixer')({
+        browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8', 'iOS >= 8', 'Android >= 4']
+      })],
       cssModules: {
         localIdentName: '[local]___[hash:base64:5]'
       },
-      rules: {
-        less: ExtractTextPlugin.extract({
-          use: 'css-loader!postcss-loader!less-loader',
-          fallback: 'vue-style-loader'
-        })
-      }
+      loaders: { js: babel }
     }
   });
   config.devServer = {
